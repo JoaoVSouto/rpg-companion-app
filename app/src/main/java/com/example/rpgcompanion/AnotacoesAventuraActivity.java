7d9e746e10ce5fd3129525d3a32287ea54352a5b
Gabriel Reis Andrade <joaogabriel2@gmail.com>
2022-02-11 18:52:34 -0300

    Created AnotacoesAventuraActivity

---
+++ app/src/main/java/com/example/rpgcompanion/AnotacoesAventuraActivity.java
@@ -0,0 +1,14 @@
+package com.example.rpgcompanion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AnotacoesAventuraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_anotacoes_aventura);
+    }
+}
