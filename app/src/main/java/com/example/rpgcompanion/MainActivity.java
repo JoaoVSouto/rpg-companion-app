83ee5bd46c34e5c5bae30c22c7a337143f99b9ef
giooogk <macedo1999.gk@gmail.com>
2022-02-12 21:17:04 -0300

    criar notificações a cada 1h

--- app/src/main/java/com/example/rpgcompanion/MainActivity.java
+++ app/src/main/java/com/example/rpgcompanion/MainActivity.java
@@ -4,13 +4,22 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.fragment.app.FragmentManager;
 
 import android.app.Activity;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -25,12 +34,17 @@ import com.example.rpgcompanion.fragmentos.FichaListaFragment;
 import com.example.rpgcompanion.fragmentos.InfoDialogFragment;
 import com.example.rpgcompanion.model.Ficha;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class MainActivity extends AppCompatActivity implements FichaListaFragment.AoClicarNaFicha,
         InfoDialogFragment.AoClicarEmInfo {
 
     private FichaListaFragment fichaListaFragment;
     private FragmentManager mFragmentManager;
     private Button btnCriarFicha;
+
+    private static final String CHANNEL_ID = "CHANNEL_ID";
+    private static final String TAG = "main";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +63,14 @@ public class MainActivity extends AppCompatActivity implements FichaListaFragmen
                 startActivity(intent);
             }
         });
+
+        createNotificationChannel();
+
+        Log.e(TAG, "onCreate");
+
+        LongOperation lo = new LongOperation(this);
+        lo.execute("Curiosidade 1", "Curiosidade 2", "Curiosidade 3", "Curiosidade 4 ", "Curiosidade 5");
+
     }
 
     @Override
@@ -150,5 +172,80 @@ public class MainActivity extends AppCompatActivity implements FichaListaFragmen
     protected void onStart() {
         this.carregarFichas();
         super.onStart();
+    }
+
+    private void createNotificationChannel() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    private class LongOperation extends AsyncTask<String, String, String> {
+
+        private static final String TAG = "longoperation";
+        private Context ctx;
+        private AtomicInteger notificationId = new AtomicInteger(0);
+
+        LongOperation(Context ctx) {
+            this.ctx = ctx;
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            for (String s : params) {
+                Log.e(TAG, s);
+
+                publishProgress(s);
+
+                for (int i = 0; i < 5; i++) {
+                    try {
+                        Thread.sleep((long) 36e+6); //(long) 36e+6 para notificações a cada 1h, 2000 para ver notificacoes mais rapido
+                    } catch (InterruptedException e) {
+                        Thread.interrupted();
+                    }
+                }
+            }
+            return "Executed";
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            for (String title: values) {
+                sendNotification(title, notificationId.incrementAndGet());
+            }
+        }
+
+        void sendNotification(String title, int notificationId) {
+
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(ctx,CHANNEL_ID)
+                    .setContentTitle(String.format("%s ", title))
+                    .setContentText("TUDO O QUE VOCÊ PRECISA SABER SOBRE RPG")
+                    .setPriority(NotificationCompat.PRIORITY_HIGH)
+                    .setAutoCancel(true)
+                    .setSmallIcon(R.drawable.ic_launcher_foreground);
+
+            Intent actionClickNotification = new Intent(ctx,MainActivity.class);
+
+            PendingIntent acaoPendente = PendingIntent.getActivity(
+                    ctx,
+                    0,
+                    actionClickNotification,
+                    PendingIntent.FLAG_UPDATE_CURRENT
+            );
+
+            builder.setContentIntent(acaoPendente);
+
+            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(ctx);
+            notificationManager.notify(notificationId, builder.build());
+        }
     }
 }
